b1a1a905e5abbdb47805c3f5ef0bb54696651992
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-09-02 17:43:09 -0400

    ope

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,6 +5,19 @@ import { babel } from '@rollup/plugin-babel';
 export default defineConfig({
   build: {
     sourcemap: true,
+    // rollupOptions: {
+    //   output: {
+    //     manualChunks: (id) => {
+    //       if (id.includes('ember-source')) {
+    //         let parts = id.split('/packages/');
+    //         return parts[1];
+    //       }
+    //     },
+    //   },
+    // },
+  },
+  optimizeDeps: {
+    exclude: ['ember-source'],
   },
   plugins: [
     ember(),
